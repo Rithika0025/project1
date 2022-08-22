d6e448e03a8b0b09e672b4b414bf3193d2567a82
rithika0025 <rithika15616@gmail.com>
2022-08-22 11:21:14 +0530

    added

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     path('master',views.master,name='master'),
-    path('home',views.home_user,name='home'),
+    path('',views.home_user,name='home'),
     path('about',views.about_user,name='about'),
     path('contact',views.contact_user,name='contact')
 ] 
